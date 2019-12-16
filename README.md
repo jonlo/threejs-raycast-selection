baf7120d607ba452635ff1e5f753af2a45824651
Jon Lopez de Guereña <jon.lopezdeguerena@tecnalia.com>
2019-12-16 14:29:38 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # threejs-raycast-selection
+
+## Classes
+
+<dl>
+<dt><a href="#Selection">Selection</a></dt>
+<dd></dd>
+</dl>
+
+## Functions
+
+<dl>
+<dt><a href="#Selection">Selection(camera)</a></dt>
+<dd><p>Creates an instance of Selection.</p>
+<p>Returns the element with the lowest userData.selectionIndex value in the intersections from a raycast</p>
+</dd>
+</dl>
+
+<a name="Selection"></a>
+
+## Selection
+**Kind**: global class
+**Version**: 1
+**Author**: jon
+<a name="Selection"></a>
+
+## Selection(camera)
+Returns the element with the lowest userData.selectionIndex value in the intersections from a raycast
+
+**Kind**: global function
+**Access**: public
+
+| Param | Type |
+| --- | --- |
+| camera | <code>Camera</code> |
---
+++ src/Selection.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * @module threejs-raycast-selection
+ * @class Selection
+ * @author jon
+ * @version 1
+ **/
+import { Scene, Raycaster } from 'three';
+
+/**
+ * Creates an instance of Selection.
+ *
+ * Returns the element with the lowest userData.selectionIndex value in the intersections from a raycast
+ * 
+ * @public
+ * @name Selection
+ * @function Selection
+ * @param {Camera}camera 
+ **/
+export class Selection {
+	constructor(camera) {
+		this.camera = camera;
+		this.raycaster = new Raycaster();
+	}
+
+	selectElement(mousePosNormalized, allElements) {
+		var intersects = this._raycastHits(this.camera, mousePosNormalized, allElements);
+		let selectedElement;
+		if (intersects.length > 0) {
+			if (!(intersects[0].object.parent instanceof Scene)) {
+				selectedElement = intersects[0].object.parent;
+			} else {
+				selectedElement = intersects[0].object;
+			}
+			try {
+				let min = Math.min.apply(Math, intersects.map((intersect) => { return intersect.object.userData.selectionIndex; }));
+				var firstElementAtIndex = intersects.find(function (o) { return o.object.userData.selectionIndex.y == min; });
+				if (firstElementAtIndex) {
+					selectedElement = firstElementAtIndex.object;
+				}
+			} catch (error) {
+				console.log(error);
+			}
+
+			return selectedElement;
+		}
+	}
+
+	_raycastHits(camera, mousePos, colliders) {
+		this.raycaster.setFromCamera(mousePos, camera);
+		let intersects = this.raycaster.intersectObjects(colliders.filter(element => element.userData.transformData.selectable));
+		return intersects;
+	}
+
+}
